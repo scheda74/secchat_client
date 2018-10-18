41b083d94534032efa4f3d4c12c385ce4cf202e7
David Schemm <davidschemm95@gmail.com>
2018-10-17 22:09:43 -0700

    working encryption and decryption.. yey

--- enc.js
+++ enc.js
@@ -12,7 +12,7 @@ function encryptMsg(msg) {
     var iv = forge.random.getBytesSync(16);
 
     var aes_ciphertext = encryptAES(iv, aes_key, msg);
-    var tag = encryptHMAC(hmac_key, aes_ciphertext);
+    var tag = runHMAC(hmac_key, aes_ciphertext);
     
     final_key = aes_key + hmac_key;
     var encrypted_key = public_key_rsa.encrypt(final_key, 'RSA-OAEP');
@@ -22,18 +22,11 @@ function encryptMsg(msg) {
         "cipher" : aes_ciphertext,
         "tag" : tag
     }
-    console.log("original hmac key: " + hmac_key);
-    
     return data;
-
-
-
-
-    //aes_plaintext = decryptAES(aes_key, aes_ciphertext);
 }
 
 
-function encryptHMAC(hmac_key, aes_ciphertext) {
+function runHMAC(hmac_key, aes_ciphertext) {
     var hmac = forge.hmac.create();
     hmac.start('sha256', hmac_key);
     hmac.update(aes_ciphertext);
@@ -61,16 +54,29 @@ function decryptMsg(data) {
     var private_key_rsa = forge.pki.privateKeyFromPem(rsa_keypair.priv);
     var decrypted_keys = private_key_rsa.decrypt(data.keys, 'RSA-OAEP');
 
-    var aes_key = decrypted_keys.slice(0, 31);
+    var aes_key = decrypted_keys.slice(0, 32);
     var hmac_key = decrypted_keys.slice(32);
 
-    console.log("aes: " + aes_key + "\n" + "hmac: " + hmac_key);
+    var tag = runHMAC(hmac_key, data.cipher);
+    if(tag === data.tag) { 
+        console.log("HMAC tags matched! Continue decryption...");
+    } else {
+        console.log("HMAC tags don't match!");
+        process.exit(1);
+    }
+    
+    //console.log("aes key: " + aes_key + " this is our key");
+
+    var plaintext = decryptAES(aes_key, data.cipher);
+    console.log(plaintext);
+
+    // console.log("hmac: " + hmac_key);
 }
 
 function decryptAES(aes_key, aes_ciphertext) {
     var iv = aes_ciphertext.slice(0, 15);
-    console.log("iv in decryption: " + iv);
-    console.log("cipher in decryption: " + aes_ciphertext.slice(16));
+    // console.log("iv in decryption: " + iv);
+    // console.log("cipher in decryption: " + aes_ciphertext.slice(16));
     var encrypted = forge.util.createBuffer(aes_ciphertext.slice(16));
 
     var decipher = forge.cipher.createDecipher('AES-CBC', aes_key);
@@ -78,7 +84,7 @@ function decryptAES(aes_key, aes_ciphertext) {
     decipher.update(encrypted);
     var result = decipher.finish(); // check 'result' for true/false
     // outputs decrypted hex
-    console.log("decrypted plaintext: " + decipher.output.data);
+    // console.log("decrypted plaintext: " + decipher.output.data);
     return decipher.output.data;
 }
 
